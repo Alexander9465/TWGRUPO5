d5f17ea94b4beac5ff1e46b6ac4de5fbcf15bf01
alexanderTupiza94 <106963893+alexanderTupiza94@users.noreply.github.com>
2022-10-05 13:53:58 +0200

    update

--- Proyecto_MyBank/src/main/java/com/MyBank/Pruebas/PruebaMyBank.java
+++ Proyecto_MyBank/src/main/java/com/MyBank/Pruebas/PruebaMyBank.java
@@ -5,16 +5,19 @@ import com.MyBank.Cuenta;
 
 public class PruebaMyBank {
 	public static void main(String[] args) {
+		int cantidad = 200;
+		int retirada = 150;
 		Cuenta cuenta1 = new Cuenta(100);
 		Cliente cliente = new Cliente("Alexander", "Tupiza", cuenta1);
 		System.out.println("el saldo de su cuenta es :");
 		System.out.println(cliente.getCuenta().getSaldo());
-		
-		cliente.getCuenta().depositar(200);
+		System.out.println("Ha depositado " + cantidad);
+		cliente.getCuenta().depositar(cantidad);
 		System.out.println("Su saldo actual es: ");
 		System.out.println(cliente.getCuenta().getSaldo());
-		cliente.getCuenta().retirada(150);
-		System.out.println(cliente.getCuenta().getSaldo());
+		System.out.println("Ha retirado "+ retirada);
+		cliente.getCuenta().retirada(retirada);
+		System.out.println("su saldo actual es:" +cliente.getCuenta().getSaldo());
 		
 		
  }
