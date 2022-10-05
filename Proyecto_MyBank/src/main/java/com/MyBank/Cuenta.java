f2ee6ca069a7e2e143db0612382399e3cca57993
alexanderTupiza94 <106963893+alexanderTupiza94@users.noreply.github.com>
2022-10-05 14:00:00 +0200

    .

--- Proyecto_MyBank/src/main/java/com/MyBank/Cuenta.java
+++ Proyecto_MyBank/src/main/java/com/MyBank/Cuenta.java
@@ -3,34 +3,36 @@ package com.MyBank;
 public class Cuenta {
 
 	private double saldo;
-	
-	public Cuenta (double saldo ) {
+
+	public Cuenta(double saldo) {
 		this.saldo = saldo;
-		
+
 	}
 
 	public double getSaldo() {
 		return saldo;
 	}
+
 	public boolean depositar(double cantidad) {
 		boolean resultado = true;
-		if(cantidad<0) throw new RuntimeException("La cantidad no puede ser menor que cero");
-		this.saldo+= cantidad;
-		
+		if (cantidad < 0)
+			throw new RuntimeException("La cantidad no puede ser menor que cero");
+		this.saldo += cantidad;
+
 		return resultado;
 	}
+
 	public boolean retirada(double cantidad) {
 		boolean resultado = false;
-		if(this.saldo>cantidad) {
+		if (this.saldo > cantidad) {
 			System.out.println("la operacion ha ido con exito ");
 			resultado = true;
-			this.saldo-=cantidad;
-		}else {
+			this.saldo -= cantidad;
+		} else {
 			System.out.println("No tienes suficiente saldo ");
 		}
-		return resultado ;
-		
+		return resultado;
+
 	}
-	
 
 }
